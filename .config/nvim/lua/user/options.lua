9c6ed6c2f418e59c164757213d4b89ac413d483f
wenhaoleng <wenhaocold@163.com>
2022-02-25 22:38:36 +0800

    nvim: add yank highlight

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -45,3 +45,10 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+
+vim.cmd [[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+  augroup end
+]]
