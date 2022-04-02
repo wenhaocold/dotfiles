61c5056e1155fbae80592dce285d1a054a4aac03
wenhaocold <wenhaocold@163.com>
2022-04-02 14:44:44 +0800

    nvim: add rainbow csv plugin

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -143,6 +143,7 @@ return packer.startup(function(use)
   use "petertriho/nvim-scrollbar"
 
   use "folke/todo-comments.nvim"
+    use 'mechatroner/rainbow_csv'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
