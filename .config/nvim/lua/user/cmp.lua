4feeb01b6ee7dad5400646407fb52e7539141fc6
wenhaocold <wenhaocold@163.com>
2022-04-14 14:53:04 +0800

    "nvim: nvim-cmp, documentation is deprecateed"

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -123,9 +123,6 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-  },
   experimental = {
     ghost_text = false,
     native_menu = false,
