bb7d715f09668b91ae0e2342c14585301eeed8fe
wenhaocold <wenhaocold@163.com>
2022-04-17 21:33:04 +0800

    nvim:

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -21,16 +21,16 @@ vim.g.maplocalleader = ","
 -- Normal --
 -- Save file
 keymap("n", "<leader>w", ":w<cr>", opts)
-keymap("i", "<leader>w", "<esc>:w<cr>", opts)
+-- keymap("i", "<leader>w", "<esc>:w<cr>", opts)
 
 -- Close buffer
 keymap("n", "<leader>q", ":Bdelete<cr>", opts)
 
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- -- Better window navigation
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-w>k", opts)
+-- keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
