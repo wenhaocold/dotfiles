38d3f6c9a2f1534760c11706c6290861213c629a
wenhaocold <wenhaocold@163.com>
2022-07-07 13:55:42 +0800

    qtile: change theme to dracula

--- qtile/.config/qtile/settings/theme.py
+++ qtile/.config/qtile/settings/theme.py
@@ -12,16 +12,15 @@ from .path import qtile_path
 
 
 def load_theme():
-    theme = "dark-grey"
+    theme = "dracula"
 
-    config = path.join(qtile_path, "config.json")
-    if path.isfile(config):
-        with open(config) as f:
-            theme = json.load(f)["theme"]
-    else:
-        with open(config, "w") as f:
-            f.write(f'{{"theme": "{theme}"}}\n')
-
+    # config = path.join(qtile_path, "config.json")
+    # if path.isfile(config):
+    #     with open(config) as f:
+    #         theme = json.load(f)["theme"]
+    # else:
+    #     with open(config, "w") as f:
+    #         f.write(f'{{"theme": "{theme}"}}\n')
 
     theme_file = path.join(qtile_path, "themes", f'{theme}.json')
     if not path.isfile(theme_file):
