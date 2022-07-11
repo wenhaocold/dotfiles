502da875b6b1d2f4e26d9d62e771a5dd0e740ef5
wenhaocold <wenhaocold@163.com>
2022-07-11 14:30:47 +0800

    nvim: remove keymaps in dashboard config

--- nvim/.config/nvim/lua/user/dashboard.lua
+++ nvim/.config/nvim/lua/user/dashboard.lua
@@ -36,16 +36,16 @@ vim.g.dashboard_custom_shortcut = {
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<leader>w", ":w<cr>", opts)
-keymap("n", "<leader>ff", ":DashboardFindFile<CR>", opts)
-keymap("n", "<leader>fg", ":Telescope live_grep<cr>", opts)
-keymap("n", "<leader>fb", ":Telescope buffers<cr>", opts)
-keymap("n", "<leader>ft", ":Telescope help_tags<cr>", opts)
-keymap("n", "<leader>fh", ":DashboardFindHistory<CR>", opts)
-keymap("n", "<leader>fc", ":DashboardChangeColorscheme<CR>", opts)
-keymap("n", "<leader>fw", ":DashboardFindWord<CR>", opts)
-keymap("n", "<leader>fm", ":DashboardJumpMark<CR>", opts)
-keymap("n", "<leader>fn", ":DashboardNewFile<CR>nmap <leader>fss :<C-u>SessionSave<CR>", opts)
-keymap("n", "<leader>fe", ":normal ,q,e<cr>", opts)
+-- keymap("n", "<leader>w", ":w<cr>", opts)
+-- keymap("n", "<leader>ff", ":DashboardFindFile<CR>", opts)
+-- keymap("n", "<leader>fg", ":Telescope live_grep<cr>", opts)
+-- keymap("n", "<leader>fb", ":Telescope buffers<cr>", opts)
+-- keymap("n", "<leader>ft", ":Telescope help_tags<cr>", opts)
+-- keymap("n", "<leader>fh", ":DashboardFindHistory<CR>", opts)
+-- keymap("n", "<leader>fc", ":DashboardChangeColorscheme<CR>", opts)
+-- keymap("n", "<leader>fw", ":DashboardFindWord<CR>", opts)
+-- keymap("n", "<leader>fm", ":DashboardJumpMark<CR>", opts)
+-- keymap("n", "<leader>fn", ":DashboardNewFile<CR>nmap <leader>fss :<C-u>SessionSave<CR>", opts)
+-- keymap("n", "<leader>fe", ":normal ,q,e<cr>", opts)
 
 
