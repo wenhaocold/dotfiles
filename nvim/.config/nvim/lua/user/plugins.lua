1cf0b18d7e8ea5dee8c31e2587c33faf60ba17d2
wenhaocold <wenhaocold@163.com>
2022-07-17 13:48:39 +0800

    nvim: add shade

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -159,6 +159,8 @@ return packer.startup(function(use)
   use 'lervag/vimtex'
 
   use "folke/lua-dev.nvim"
+  
+  use 'sunjon/shade.nvim'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
