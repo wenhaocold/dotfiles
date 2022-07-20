b41dd40a17a02ef74190358f7ba3464e86a7774f
wenhaocold <wenhaocold@163.com>
2022-07-20 13:12:49 +0800

    nvim: twilight

--- nvim/.config/nvim/lua/user/twilight.lua
+++ nvim/.config/nvim/lua/user/twilight.lua
@@ -5,17 +5,16 @@ require("twilight").setup {
     color = { "Normal", "#ffffff" },
     inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
   },
-  context = 10, -- amount of lines we will try to show around the current line
+  context = 20, -- amount of lines we will try to show around the current line
   treesitter = true, -- use treesitter when available for the filetype
   -- treesitter is used to automatically expand the visible text,
   -- but you can further control the types of nodes that should always be fully expanded
   expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
     "function_definition",
-    -- "identifier",
-    -- "function",
-    -- "method",
-    -- "table",
-    -- "if_statement",
+    "function",
+    "method",
+    "table",
+    "if_statement",
   },
   exclude = {}, -- exclude these filetypes
 }
