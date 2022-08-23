d32219c58a4f9d7b8152a94ffbacbb3290068d00
wenhaocold <wenhaocold@163.com>
2022-08-23 19:55:56 +0800

    nvim: change default icon

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -21,6 +21,35 @@
 --   },
 -- }
 
+
+-- change default icon
+local status_ok, nvim_web_devicons = pcall(require, "nvim-web-devicons")
+if not status_ok then
+  return
+end
+nvim_web_devicons.setup {
+ -- your personnal icons can go here (to override)
+ -- you can specify color or cterm_color instead of specifying both of them
+ -- DevIcon will be appended to `name`
+ override = {
+  py = {
+    icon = "",
+    color = "#6398B7",
+    cterm_color = "65",
+    name = "Python"
+  },
+  Makefile = {
+    icon = "",
+    color = "#E37834",
+    cterm_color = "65",
+    name = "Makefile"
+  },
+ };
+ -- globally enable default icons (default to false)
+ -- will get overriden by `get_icons` option
+ default = true;
+}
+
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
   return
