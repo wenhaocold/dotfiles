e25d644f2c03c8c949889e4040dfa604c4bfc6ac
wenhaocold <wenhaocold@163.com>
2022-10-30 14:31:19 +0800

    nvim: change the keybinding of closing nvim-cmp panel

--- nvim/.config/nvim/lua/user/cmp.lua
+++ nvim/.config/nvim/lua/user/cmp.lua
@@ -66,6 +66,10 @@ cmp.setup {
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     },
+    ["<C-l>"] = cmp.mapping {
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    },
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
