261e64c6235ac2b3c8fe55ed891fcd6496f513cb
wenhaocold <wenhaocold@163.com>
2022-11-30 18:22:31 +0800

    zsh: update cpolar's address

--- zsh/.config/zsh/command.zsh
+++ zsh/.config/zsh/command.zsh
@@ -3,17 +3,17 @@
 if command -v cpolar >/dev/null; then
     cpolar-ssh ()
     {
-        cpolar tcp 22 -remote-addr=1.tcp.cpolar.cn:24737
+        cpolar tcp 22 -remote-addr=1.tcp.vip.cpolar.top:11022
     }
 
-    cpolar-proxy ()
-    {
-        cpolar tcp 7890 -remote-addr=1.tcp.vip.cpolar.top:10327
-    }
+    # cpolar-proxy ()
+    # {
+    #     cpolar tcp 7890 -remote-addr=1.tcp.vip.cpolar.top:10327
+    # }
 
     cpolar-vnc ()
     {
-        cpolar tcp 5901 -remote-addr=1.tcp.cpolar.cn:22252
+        cpolar tcp 5901 -remote-addr=1.tcp.vip.cpolar.top:10168
     }
 fi
 
