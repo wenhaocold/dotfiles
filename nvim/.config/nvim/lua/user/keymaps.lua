72dbfe1bccea5d9d90f72114bbe502e58cec7ab7
wenhaocold <wenhaocold@163.com>
2023-02-13 16:59:25 +0800

    nvim: change some keybindings

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -43,18 +43,18 @@ vim.keymap.set('n', "<A-j>", '<CMD>NavigatorDown<CR>')
 vim.keymap.set('n', "<A-p>", '<CMD>NavigatorPrevious<CR>')
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<A-Up>", ":resize +2<CR>", opts)
+keymap("n", "<A-Down>", ":resize -2<CR>", opts)
+keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+--[[ keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts) ]]
+--[[ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts) ]]
 
 -- Insert --
 -- Press jk fast to enter
@@ -67,16 +67,16 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+--[[ keymap("v", "<A-j>", ":m .+1<CR>==", opts) ]]
+--[[ keymap("v", "<A-k>", ":m .-2<CR>==", opts) ]]
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+--[[ keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts) ]]
+--[[ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts) ]]
 
 -- Terminal --
 -- Better terminal navigation
